034bb01fc8d8559e84d691ebf259c79a0ba1797f
Diego Urrego <diegourrg@gmail.com>
2024-11-15 23:18:54 -0500

    feat: section 15 ended.

--- sections-exercises/section-15.sql
+++ sections-exercises/section-15.sql
@@ -48,3 +48,42 @@ CALL insert_region_proc(5, 'Central America');
 
 SELECT *
 FROM regions;
+
+SELECT CURRENT_DATE                  AS raise_date,
+       employee_id,
+       salary,
+       max_raise(employee_id) * 0.05 AS amount,
+       5                             AS percentage
+FROM employees;
+
+-- Percentage: 5
+CREATE OR REPLACE PROCEDURE controlled_raise(percentage NUMERIC) AS
+$$
+DECLARE
+    real_percentage NUMERIC(8, 2);
+    total_employees INT;
+BEGIN
+    real_percentage = percentage / 100;
+
+    -- Mantener el historic
+    INSERT INTO raise_history(date, employee_id, base_salary, amount, percentage)
+    SELECT CURRENT_DATE                             AS raise_date,
+           employee_id,
+           salary,
+           max_raise(employee_id) * real_percentage AS amount,
+           percentage                               AS percentage
+    FROM employees;
+
+    -- Impactar la tabla de empleados
+    UPDATE employees
+    SET salary = max_raise(employee_id) * real_percentage + salary;
+
+    COMMIT;
+
+    SELECT count(*) into total_employees FROM employees;
+    RAISE NOTICE 'Afectados % empleados', total_employees;
+
+END;
+$$ LANGUAGE plpgsql;
+
+CALL controlled_raise(1);
